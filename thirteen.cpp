9a135c9a0a623e17162faa5fe5d2266818c44c84
Onyebuchi Obi <oobi@usc.edu>
2018-10-05 20:34:26 -0700

    Create thirteen.cpp

---
+++ thirteen.cpp
@@ -0,0 +1,19 @@
+/*
+Complete this program so that when the input x is thirteen, the program prints out Unlucky and otherwise, does nothing. 
+Hint: use if. 
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+   int x;
+   cin >> x;
+   // if x is equal to thirteen, print out "Unlucky"
+    if(x == 13)
+        cout << "Unlucky" << endl;
+
+
+
+
+   return 0;
+}
