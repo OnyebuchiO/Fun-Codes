0e33bbf6b270b3f7fb836d88d87e3a839440e007
Onyebuchi Obi <oobi@usc.edu>
2018-10-05 20:31:46 -0700

    Create absolute.cpp

---
+++ absolute.cpp
@@ -0,0 +1,24 @@
+/*
+Complete this program so that when the input x is positive, it prints x, and when it is negative, it prints -x. 
+Hint: use else.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+   int x;
+   cin >> x;
+   // print out either x or negative x
+    if(x>=0)
+        cout << x << endl;
+    else
+        cout << x << endl;
+
+
+
+
+
+
+   return 0;
+}
